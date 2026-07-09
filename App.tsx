c0d5beecb1b49d89c96ac9ebb168d62474225925
André Luiz Perensin <andreluizperensin@gmail.com>
2026-07-09 17:04:57 -0300

    update app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import { NotesProvider } from './src/context/NotesContext';
+
 import { ThemeProvider } from './src/context/ThemeContext';
+
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 function App(): React.JSX.Element {
